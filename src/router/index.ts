520e06949576c6e70db72b189c050c8cf0845653
daihaitian <daihaitian@guiji.ai>
2020-09-10 14:53:18 +0800

    新一步的类型补充学习

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Home',
     component: () => import('../views/Home.vue'),
   },
+  {
+    path: '/Test',
+    name: 'Test',
+    component: () => import('../views/Test.vue'),
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="test">
+    <div class="ceshi"></div>
+    <div>fsdafsasasasasasasasasasasasasasa</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: '',
+}
+</script>
+
+<style scoped lang="scss">
+.test {
+  //background: #000;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.ceshi {
+  position: absolute;
+  width: 82px;
+  height: 30px;
+  /*border-right: 8px solid #65a7ef;*/
+  /*border-bottom: 4.5px solid #65a7ef;*/
+  transform: rotate(-145deg) skew(51deg);
+  box-shadow: red 0 0 6px inset;
+  clip: rect(10px 82px 31px 30px);
+}
+</style>
