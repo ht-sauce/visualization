a839fbc9824a5283f531b08d8dbe3a88f9a287d8
daihaitian <daihaitian@guiji.ai>
2020-12-30 14:08:11 +0800

    jsx示例编写

--- src/views/Experiment/Test.tsx
+++ src/views/Experiment/Test.tsx
@@ -1,4 +1,5 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, reactive } from 'vue'
+import { VNode, FunctionalComponent } from '@vue/runtime-core'
 
 const TestA = defineComponent({
   setup(props, ctx) {
@@ -16,6 +17,59 @@ const TestD = defineComponent({
             default: (n: string) => <span>{n}</span>,
           }}
         />
+      </div>
+    )
+  },
+})
+
+const Testref = defineComponent({
+  setup(props, ctx) {
+    const dateone = ref('测试')
+    const datetwo = reactive({
+      test: '测试',
+    })
+    return () => (
+      <div>
+        <div>{dateone}</div>
+        <div>{datetwo.test}</div>
+      </div>
+    )
+  },
+})
+
+const iffor = defineComponent({
+  setup(props, ctx) {
+    const ces = true
+    const ifelse = () => {
+      if (ces) return <div>if部分</div>
+      else return <div>else部分</div>
+    }
+
+    const forarr = () => {
+      let i = 0
+      return new Array(10).fill('').map(() => {
+        i++
+        return <span>{i}</span>
+      })
+    }
+
+    return () => (
+      <div>
+        {ifelse()}
+        {forarr()}
+      </div>
+    )
+  },
+})
+
+const testslot = defineComponent({
+  setup(props, ctx) {
+    return () => (
+      <div>
+        {/*默认插槽*/}
+        <div>{ctx.slots.default && ctx.slots.default()}</div>
+        {/*具名插槽*/}
+        <div>{ctx.slots.name && ctx.slots.name()}</div>
       </div>
     )
   },
--- src/views/Experiment/index.vue
+++ src/views/Experiment/index.vue
@@ -4,26 +4,42 @@
       <template #default="de">{{ de }}</template>
     </test-com>
     <test-a></test-a>
+    <NoDcom test="测试props" />
+    <div>
+      <slot></slot>
+    </div>
+    <div>
+      <slot name="name"></slot>
+    </div>
   </div>
 </template>
 
 <script lang="tsx">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref, toRefs } from 'vue'
 import { TestCom } from './Test'
+import NoDcom from './NoDcom'
 
 const TestA = () => <div>fdsafdsa</div>
 
 export default defineComponent({
-  components: { TestCom, TestA },
+  components: { TestCom, TestA, NoDcom },
   setup(props, ctx) {
-    return {}
+    const dateone = ref('测试')
+    const datetwo = reactive({
+      test: '测试',
+    })
+    return {
+      dateone,
+      ...toRefs(datetwo),
+    }
   },
 })
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .experiment {
   display: flex;
+  flex-flow: column;
   align-items: center;
   justify-content: center;
 }
