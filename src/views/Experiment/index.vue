c03b2c9e84dc3e5d70ad2ca3786d52b3c328c248
dht <603928588@qq.com>
2022-09-21 09:48:33 +0800

    测试文件黏贴

--- src/views/Experiment/index.vue
+++ src/views/Experiment/index.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="test-content">
     <dht-button style="width: 200px">测试文件上传</dht-button>
-    <div @paste="onPaste" class="tarea" placeholder="文件内容">
+    <div
+      tabindex="1"
+      @focus="onfocus"
+      @blur="onblur"
+      @paste="onPaste"
+      class="tarea"
+      placeholder="文件内容"
+    >
       <template v-for="(item, index) in imgList" :key="index">
-        <img :src="item" />
+        <!--        <img :src="item" />-->
+        <span>{{ item }}</span>
       </template>
     </div>
   </div>
@@ -20,21 +28,29 @@ function onPaste(e: ClipboardEvent) {
   const items = e.clipboardData?.items
   if (items && items.length) {
     for (let i = 0; i < items.length; i++) {
-      if (items[i].type.indexOf('image') !== -1) {
-        file = items[i].getAsFile()
-        break
-      }
+      file = items[i].getAsFile()
+      // if (items[i].type.indexOf('image') !== -1) {
+      //   file = items[i].getAsFile()
+      //   break
+      // }
     }
   }
   if (!file) {
     console.log('粘贴的不是图片')
     return
   }
+  console.log(file.name, file)
   imgList.value.push(URL.createObjectURL(file))
   // 当成正常的input文件上传处理
   // 如下
   // const fromData = new FormData()
   // fromData.append(file.name, file)
+}
+function onfocus() {
+  console.log(1)
+}
+function onblur() {
+  console.log(2)
 }
 </script>
 
